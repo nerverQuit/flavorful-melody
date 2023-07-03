72e36b1a740686ea1d7a7cbfd14e8ab2385ce70e
Thomas Sang <40707686+uiseongsang@users.noreply.github.com>
2023-07-03 11:49:41 +0900

    init commit

---
+++ src/main/java/com/example/flavorfulmelody/FlavorfulMelodyApplication.java
@@ -0,0 +1,13 @@
+package com.example.flavorfulmelody;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlavorfulMelodyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlavorfulMelodyApplication.class, args);
+    }
+
+}
