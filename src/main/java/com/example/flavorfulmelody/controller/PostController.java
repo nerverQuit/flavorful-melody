32e0748a58391b3bf6496bdb431eaebe9c1dd692
hjern <hjern.refrain@gmail.com>
2023-07-05 15:16:23 +0900

    Annotation 추가 + api 주소 변경

--- src/main/java/com/example/flavorfulmelody/controller/PostController.java
+++ src/main/java/com/example/flavorfulmelody/controller/PostController.java
@@ -2,14 +2,7 @@ package com.example.flavorfulmelody.controller;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.flavorfulmelody.dto.PostRequestDto;
 import com.example.flavorfulmelody.dto.PostResponseDto;
@@ -30,7 +23,7 @@ public class PostController {
 	}
 
 	// id 로 선택된 게시물 조회
-	@GetMapping("/posts/{post_id}")
+	@GetMapping("/posts/{id}")
 	public PostResponseDto getPost(@PathVariable Long id){
 		return postService.getPost(id);
 	}
@@ -42,12 +35,12 @@ public class PostController {
 	}
 
 	// 선택한 게시글 수정(변경)
-	@PutMapping("/posts/{post_id}")
+	@PutMapping("/posts/{id}")
 	public PostResponseDto updatePost (@PathVariable Long id, @RequestBody PostRequestDto requestDto) {
 		return postService.updatePost(id, requestDto);
 	}
 
-	@DeleteMapping("/posts/{post_id}")
+	@DeleteMapping("/posts/{id}")
 	public PostResponseDto deletePost(@PathVariable Long id) {
 		return postService.deletePost(id);
 	}
--- src/main/java/com/example/flavorfulmelody/repository/PostRepository.java
+++ src/main/java/com/example/flavorfulmelody/repository/PostRepository.java
@@ -3,9 +3,11 @@ package com.example.flavorfulmelody.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.flavorfulmelody.entity.Post;
 
+@Repository
 public interface PostRepository extends JpaRepository<Post,Long> {
 	List<Post> findAllByOrderByCreatedAtDesc();
 
