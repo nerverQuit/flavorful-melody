5cff2842e97110e860efb70e0ca4f769868e4b2e
Thomas Sang <40707686+uiseongsang@users.noreply.github.com>
2023-07-06 10:11:22 +0900

    댓글 기능 병합 완료

--- src/main/java/com/example/flavorfulmelody/entity/Post.java
+++ src/main/java/com/example/flavorfulmelody/entity/Post.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -26,7 +27,7 @@ public class Post extends Timestamped {
 	private User user;
 
 	@OneToMany(mappedBy = "post", cascade = CascadeType.REMOVE)
-	private List<Comment> comments;
+	private List<Comment> comments = new ArrayList<>();
 
 	// @Column(name = "likeCnt", nullable = false)
 	// private Long likeCnt;
